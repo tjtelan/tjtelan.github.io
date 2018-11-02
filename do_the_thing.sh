94229a2b00ec6e65d255e60a988734cc210a5372
T.J. Telan <tjtelan@level11.com>
2018-11-02 15:15:31 -0700

    Fixing pipenv command for deploying

--- do_the_thing.sh
+++ do_the_thing.sh
@@ -1,4 +1,4 @@
 # Don't actually run this script right now.
 #!/usr/bin/env/ sh
 gutenberg build
-pipenv shell run ghp-import -n -c tjtelan.com -b master -m "${1}" -p public
+pipenv run ghp-import -n -c tjtelan.com -b master -m "${1}" -p public
