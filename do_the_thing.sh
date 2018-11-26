6cb69b50275280e1c4c1fdd9a4b4443a02736d8f
T.J. Telan <tjtelan@level11.com>
2018-11-26 13:41:57 -0800

    Updating the deployment script to be easier to read, and to check for build issues before trying to push.

--- do_the_thing.sh
+++ do_the_thing.sh
@@ -1,10 +1,25 @@
-# Don't actually run this script right now.
 #!/usr/bin/env/ sh
+
+set -e
 
 if [[ $# -eq 0 ]] ; then
     echo 'Please provide a quoted commit message'
-    exit 0
+    exit 1
+fi
+COMMIT_MSG="${1}"
+
+# Compile the zola site using the docker container
+docker-compose run zola zola build
+
+# Don't publish if the build fails
+if [[ $? -ne 0 ]] ; then
+    exit 1
 fi
 
-docker-compose run zola zola build
-pipenv run ghp-import -n -c tjtelan.com -b master -m "${1}" -p public
+GITHUB_PUBLISH_REPO=master
+ZOLA_BUILD_DIR=./public
+CNAME=tjtelan.com
+
+# TODO: Move this pipenv code into the docker container to keep tools off the host
+# Publish the locally compiled site to github
+pipenv run ghp-import -n -c ${CNAME} -b ${GITHUB_PUBLISH_REPO} -m ${COMMIT_MSG} -p ${ZOLA_BUILD_DIR}
